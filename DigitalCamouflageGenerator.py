de561d417d584bf196f30b10817062b2983c51ae
OEG <oeg8168@gmail.com>
2016-11-28 21:14:16 +0800

    add parseArguments() for command line arguments

--- DigitalCamouflageGenerator.py
+++ DigitalCamouflageGenerator.py
@@ -1,33 +1,70 @@
 from camoImageFactory import *
 
+import sys
+import getopt
 import random
 
-def main():
+def main(argv):
+
+    width, height, camoImgNumber, outputFile = parseArguments(argv)
+
+    print(width, height, camoImgNumber, outputFile)
+    
     camoImageFactory = CamoImageFactory()
 
-    canvasSize = (200, 200)
+    canvasSize = (width, height)
     canvas = Image.new('LA', canvasSize, 'white')
 
-    camoImgNumber = 1000
     for i in range(camoImgNumber):
         camoImg = camoImageFactory.getCamoImage()
         
         img = camoImg.toImageObj()
-        x = random.randint(-100, 200)
-        y = random.randint(-100, 200)
+        x = random.randint(-camoImg.col, width)
+        y = random.randint(-camoImg.row, height)
 
         canvas.paste(img, (x, y), img.split()[1])
-
-        # camoImg.saveToPNG()
     
-    canvas.save('output.png')
+    canvas.save(outputFile)
 
+
+def parseArguments(argv):
+    
+    try:
+        opts, args = getopt.getopt(argv, 'w:h:n:o:')
+    except getopt.GetoptError as e:
+        print('error:', e)
+        printUsage()
+        exit(1)
+
+    # defaults
+    width, height = 200, 200
+    camoImgNumber = 1000
+    outputFile = 'output.png'
+
+    try:
+        for opt, arg in opts:
+            print(opt, arg)
+            if opt in '-w':
+                width = int(arg)
+            elif opt in '-h':
+                height = int(arg)
+            elif opt in '-n':
+                camoImgNumber = int(arg)
+            elif opt in '-o':
+                outputFile = arg
+    except Exception as e:
+        print(e)
+        exit(1)
+    
+    return width, height, camoImgNumber, outputFile
+
+
+def printUsage():
+    print('Usage: python DigitalCamouflageGenerator.py')
+    print('       [-w <width>] [-h <height>]')
+    print('       [-n <numberOfCamo>] [-r <ruggedness>]')
+    print('       [-o <outputFile>]')
+    
 
 if __name__ == '__main__':
-    main()
-
-    '''
-    for i in range(100):
-        camoImg = camoImageFactory.getCamoImage()
-        camoImg.saveToPNG('./out/' + str(i) + '.png')
-    '''
+    main(sys.argv[1:])
